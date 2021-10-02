b3ee13ab6eccfe47ac3ecd111b87ef4c9bf7dd3a
Yazeed Almuqwishi <yazeed.almuqwishi@gmail.com>
2021-10-02 12:58:26 +0300

    Merge bajo's changes

--- js/reform.js
+++ js/reform.js
@@ -1,7 +1,10 @@
 var districts = {};
 var apikey = "pk.eyJ1IjoiYWJvb2R6YmFqbyIsImEiOiJja3U1d21hNDEyYXJ4Mm5wYzJub3V5MGdlIn0.QVGymFsf-n1A-8i1LK1y0g";
-var mymap = L.map('map');
+var mymap = L.map('map').setView([26.3726215,50.0855243],12);
+// var mymap = L.map('map').setView([26.4091858,50.0876064],12);
+
 var mylocation = null;
+var indices = ['fast_internet_index', 'infrastructure_index', 'hospitals_index', 'pharmacies_index', 'mosques_index', 'schools_index', 'population_density_index']
 
 load_districts();
 
@@ -13,10 +16,11 @@ L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
     accessToken: apikey,
 }).addTo(mymap);
 
-mymap.locate({
-    setView: true,
-    maxZoom: 14,
-});
+
+// mymap.locate({
+//     setView: true,
+//     maxZoom: 14,
+// });
 
 
 
@@ -32,7 +36,7 @@ document.getElementById("locateMe").addEventListener('click', () => {
 
 document.addEventListener("click", (event) => {
     if (event.target.classList[0] in districts) {
-        updateLegend(districts[event.target.classList[0]]);
+        updateLegend(districts[event.target.classList[0]],event.target.classList[0]);
     }
 })
 /*************************************************************** 
@@ -47,11 +51,11 @@ function locateMe(mymap) {
         mymap.setView(latlng, 15);
 
         mylocation = new L.Marker(latlng, {
-            style: { className: "hi" },
             icon: L.icon({
                 iconUrl: '../img/home.svg',
                 iconSize: [30, 30],
                 shadowSize: [50, 64],
+                iconAnchor: [15,15]
             })
         }).addTo(mymap);
     });
@@ -72,26 +76,31 @@ function colorRange(value) {
     return '#' + ('000000' + h.toString(16)).slice(-6);
 }
 
-function load_districts() {
+function load_districts() { 
     var xhr = new XMLHttpRequest();
     xhr.onreadystatechange = function () {
         if (xhr.readyState == 4 && xhr.status === 200 && xhr.DONE) {
-            L.geoJson(JSON.parse(xhr.responseText), { style: areaStyle, onEachFeature: makeLabel }).addTo(mymap);
+            L.geoJson(JSON.parse(xhr.responseText), { style: areaStyle, /*onEachFeature: makeLabel*/ }).addTo(mymap);
         }
     };
-    xhr.open("GET", "api/", true);
+    // xhr.open("GET", "./districts", true);
+    xhr.open("GET","api/dammam",true);
     xhr.send();
 }
 
-function getAreaColor(feature) {
-    var water = parseInt(feature.properties.water);
-    var electricity = parseInt(feature.properties.electricity);
-    var parking = parseInt(feature.properties.parking);
 
-    return colorRange(((water + electricity + parking) / 300) * 100);
+function getAreaColor(feature) {
+    // console.log(feature.properties)
+    var total = 0;
+    for(var idx=0; idx < indices.length; idx++){
+        total +=parseFloat(feature.properties[indices[idx]])
+    }
+    // console.log('total percent', (total/700.0)*100.0)
+    return colorRange((total/(700.0))*100.0);
 };
 
 function areaStyle(feature) {
+    addToDistricts(feature);
     return {
         fillColor: getAreaColor(feature),
         weight: 2,
@@ -99,39 +108,40 @@ function areaStyle(feature) {
         color: 'white',
         dashArray: '3',
         fillOpacity: 0.5,
-        className: feature.properties.name,
+        className: districtClassName(feature.properties['name_en']),
     }
 };
 
 function makeLabel(feature, layer) {
     var htmlLabel = `
-    <b>${feature.properties.name}</b><br>
-    <b>${feature.properties['name-ar']}</b><br>
-    <img src="../img/${feature.properties.img}" width="100px">
+    <b>${feature.properties['name_en']}</b><br>
+    <b>${feature.properties['name_ar']}</b><br>
     `;
     L.marker(layer.getBounds().getCenter()).bindPopup(htmlLabel).addTo(mymap);
 
-    addToDistricts(feature);
 }
 
 function addToDistricts(feature) {
-    var water = parseInt(feature.properties.water);
-    var electricity = parseInt(feature.properties.electricity);
-    var parking = parseInt(feature.properties.parking);
-    districts[feature.properties.name] = { "parking": parking, "electricity": electricity, "water": water };
+    // console.log(feature.properties['name_en'])
+    districts[districtClassName(feature.properties['name_en'])]={}
+    for(var idx =0; idx < indices.length; idx++){
+        districts[districtClassName(feature.properties['name_en'])][indices[idx]]=feature.properties[indices[idx]];
+    }
 }
 
-function updateLegend(district) {
-    //parking
-    //electricity
-    //water
+function updateLegend(district, districtName) {
+    $('#district_english').text(districtName.replaceAll("_"," "));
     $('.indicator').each((index, obj) => {
         var indicator = Object.keys(district)[index];
         obj.style.width = `${(district[indicator])}%`;
         obj.style.backgroundColor = colorRange(district[indicator]);
+
     })
 }
 
+function districtClassName(str){
+    return str.trim().replaceAll(" ","_").replaceAll(".","");
+}
 
 
 
